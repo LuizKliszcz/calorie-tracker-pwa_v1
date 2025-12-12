4063b606a1f96ff63b1e278f47ef9ff2317088b7
Luiz Henrique Kliszcz <88855584+LuizKliszcz@users.noreply.github.com>
2025-12-12 11:14:37 -0300

    Create foods.js

---
+++ src/foods.js
@@ -0,0 +1,15 @@
+export const FOODS = [
+  { name: "Arroz branco cozido", portion: "100g", kcal: 128 },
+  { name: "Feijão carioca cozido", portion: "100g", kcal: 76 },
+  { name: "Frango grelhado", portion: "100g", kcal: 165 },
+  { name: "Alcatra em tiras", portion: "100g", kcal: 215 },
+  { name: "Batata inglesa cozida", portion: "100g", kcal: 87 },
+  { name: "Batata doce cozida", portion: "100g", kcal: 86 },
+  { name: "Ovo cozido", portion: "1 unid", kcal: 78 },
+  { name: "Banana prata", portion: "1 unid", kcal: 90 },
+  { name: "Morango", portion: "100g", kcal: 32 },
+  { name: "Leite condensado", portion: "1 colher sopa", kcal: 62 },
+  { name: "Pão francês", portion: "1 unid", kcal: 135 },
+  { name: "Macarrão cozido", portion: "100g", kcal: 157 },
+  { name: "Carne moída cozida", portion: "100g", kcal: 240 }
+]
